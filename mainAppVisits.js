fd9172106d49de8fa7e57b6742dfb2788bc01f17
Masdedra <44213771+Masdedra@users.noreply.github.com>
2024-10-13 16:07:01 -0600

    Update mainAppVisits.js

--- mainAppVisits.js
+++ mainAppVisits.js
@@ -16,7 +16,7 @@ const io = new Server(server);
 const fs = require('fs');
 
 var itemDB = new Object();
-fs.readFile('/root/Database.json', function (error, content) {
+fs.readFile('/home/ec2-user/PetFiles/Database.json', function (error, content) {
 	itemDB = JSON.parse(content);
 });
 
@@ -43,16 +43,6 @@ function sleep(ms) {
 	});
 }
 
-// const ProxyAgent = require('https-proxy-agent')
-const ProxyAgent = require('proxy-agent')
-const opts = {
-	transports: ['websocket'],
-	reconnect: true,
-	// secure: true,
-	// rejectUnauthorized:false,
-	// agent: new ProxyAgent('http://localhost:8888')
-	agent: new ProxyAgent('http://localhost:8888')
-}
 
 
 function getObjKey(obj, value) {
@@ -103,12 +93,11 @@ function countProperties(obj) {
 }
 
 const config = {
-	"host": "localhost",
-	"user": "root",
+	"host": "93.127.197.124",
+	"user": "Admin",
 	"password": "danielito001",
 	"base": "temporal_pet"
 };
-
 var db = mysql.createConnection({
 	host: config.host,
 	user: config.user,
